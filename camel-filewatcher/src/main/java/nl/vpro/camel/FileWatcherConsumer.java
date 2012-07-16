cea2753ba7933e3f10730b47a428cff209de72f0
soarten <soarten@vpro.nl>
2012-07-16 07:05:38 +0000

    camel parent project

---
+++ camel-filewatcher/src/main/java/nl/vpro/camel/FileWatcherConsumer.java
@@ -0,0 +1,159 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.vpro.camel;
+
+import java.io.File;
+import java.net.URI;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.impl.DefaultScheduledPollConsumer;
+import org.apache.camel.util.FileUtil;
+
+public class FileWatcherConsumer extends DefaultScheduledPollConsumer {
+    public static enum Event {
+        STARTED,CREATED,UPDATED,DELETED;
+    }
+
+    private final FileWatcherEndpoint endpoint;
+
+    private String watchedFilePath;
+
+    private File watchedFile;
+
+    private long previousLastModified = 0;
+
+    private boolean firstPoll = true;
+
+    public FileWatcherConsumer(FileWatcherEndpoint endpoint, Processor processor) {
+        super(endpoint, processor);
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    protected int poll() throws Exception {
+        if(noFileSinceStartup()) {
+            firstPoll = false;
+            return 0;
+        }
+
+        if(previousFileWasDeleted()) {
+            exchangeFileDeleted();
+            // do not update timestamp when an exception is not handled
+            previousLastModified = 0;
+            return 1;
+        }
+
+        final long newLastModified = watchedFile.lastModified();
+
+        if(notModified(newLastModified)) {
+            return 0;
+        }
+
+        Event event;
+        if(routeStartedOrFileCreated()) {
+            if(firstPoll) {
+                event = Event.STARTED;
+            } else {
+                event = Event.CREATED;
+            }
+        } else {
+            event = Event.UPDATED;
+        }
+
+        exchangeFile(event);
+        // do not update fields when an exception is not handled
+        this.previousLastModified = newLastModified;
+        this.firstPoll = false;
+        return 1;
+    }
+
+    @Override
+    protected void doStart() throws Exception {
+        watchedFilePath = resolvePath();
+        watchedFile = new File(watchedFilePath);
+
+        if(watchedFile.isDirectory()) {
+            throw new IllegalArgumentException("Endpoint " + endpoint + " should not point to a directory + " + watchedFile.getAbsolutePath());
+        }
+
+        if(watchedFile.exists() && !watchedFile.canRead()) {
+            throw new SecurityException("Can not read file: " + watchedFile.getAbsolutePath());
+        }
+
+        super.doStart();
+    }
+
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+        previousLastModified = 0;
+        firstPoll=true;
+    }
+
+    private boolean noFileSinceStartup() {
+        return previousLastModified == 0 && !watchedFile.exists();
+    }
+
+    private boolean previousFileWasDeleted() {
+        return previousLastModified > 0 && !watchedFile.exists();
+    }
+
+    private boolean notModified(long newLastModified) {
+        return newLastModified <= this.previousLastModified;
+    }
+
+    private boolean routeStartedOrFileCreated() {
+        return this.previousLastModified == 0 && watchedFile.exists();
+    }
+
+    private String resolvePath() {
+        URI uri = endpoint.getEndpointConfiguration().getURI();
+        String authority = uri.getAuthority();
+        String path = uri.getPath();
+
+        if(authority == null) {
+            return FileUtil.normalizePath(path);  // absolute path
+        }
+
+        return FileUtil.normalizePath(authority + path);
+    }
+
+    private void exchangeFileDeleted() throws Exception {
+        Exchange exchange = endpoint.createExchange();
+        exchange.getIn().setBody(null);
+        exchange.getIn().setHeader("fileWatchEvent", Event.DELETED.name());
+        process(exchange);
+    }
+
+    private void exchangeFile(Event event) throws Exception {
+        Exchange exchange = endpoint.createExchange();
+        exchange.getIn().setHeader("fileWatchEvent", event.name());
+        exchange.getIn().setBody(watchedFile);
+        process(exchange);
+    }
+
+    private void process(Exchange exchange) throws Exception {
+        try {
+            getProcessor().process(exchange);
+        } finally {
+            if(exchange.getException() != null) {
+                getExceptionHandler().handleException("Error processing exchange", exchange, exchange.getException());
+            }
+        }
+    }
+}
---
+++ camel-filewatcher/src/test/java/nl/vpro/camel/FileWatcherComponentTest.java
@@ -0,0 +1,118 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.vpro.camel;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FileWatcherComponentTest extends CamelTestSupport {
+
+    private static final String filePath = "fileWatcherTest.txt";
+
+    private static File file;
+
+    @BeforeClass
+    public static void createFile() throws Exception {
+        file = new File(filePath);
+        if(file.exists()) {
+            throw new IllegalStateException("A test(?) file exists already at: " + file.getAbsolutePath());
+        }
+
+        file.createNewFile();
+        FileWriter writer = new FileWriter(file, true);
+        writer.write("Hello world");
+        writer.close();
+    }
+
+    @AfterClass
+    public static void deleteFile() {
+        if(file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    public void testFileWatcherForAllEvents() throws Exception {
+        // Started
+        MockEndpoint mock = getMockEndpoint("mock:result");
+        mock.expectedMinimumMessageCount(1);
+        List<Exchange> exchanges = mock.getExchanges();
+
+        assertMockEndpointsSatisfied();
+        assertInMessageHeader(exchanges.get(0), "fileWatchEvent", "STARTED");
+        assertInMessageBodyEquals(exchanges.get(0), "Hello world");
+
+        // Updated
+        resetMocks();
+
+        FileWriter w1 = new FileWriter(file);
+        w1.write("Update");
+        w1.close();
+
+        mock.expectedMinimumMessageCount(1);
+        exchanges = mock.getExchanges();
+
+        assertMockEndpointsSatisfied();
+        assertInMessageHeader(exchanges.get(0), "fileWatchEvent", "UPDATED");
+        assertInMessageBodyEquals(exchanges.get(0), "Update");
+
+        // Deleted
+        resetMocks();
+
+        file.delete();
+
+        mock.expectedMinimumMessageCount(1);
+        exchanges = mock.getExchanges();
+
+        assertMockEndpointsSatisfied();
+        assertInMessageHeader(exchanges.get(0), "fileWatchEvent", "DELETED");
+        assertNull(exchanges.get(0).getIn().getBody());
+
+        // Created
+        resetMocks();
+
+        FileWriter w2 = new FileWriter(file);
+        w2.write("Created");
+        w2.close();
+
+        mock.expectedMinimumMessageCount(1);
+        exchanges = mock.getExchanges();
+
+        assertMockEndpointsSatisfied();
+        assertInMessageHeader(exchanges.get(0), "fileWatchEvent", "CREATED");
+        assertInMessageBodyEquals(exchanges.get(0), "Created");
+    }
+
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception {
+        return new RouteBuilder() {
+            public void configure() {
+                from("filewatcher:" + filePath)
+                    .to("mock:result");
+            }
+        };
+    }
+}
